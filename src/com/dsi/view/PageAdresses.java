31da09f9ca2329d72741fed1de2bbc75a61db1ea
alexismoquet <alexismoquet@gmail.com>
2020-09-28 17:28:15 +0200

    btnAdresse rouge su user n'a pas d'adresse

--- src/com/dsi/view/PageAdresses.java
+++ src/com/dsi/view/PageAdresses.java
@@ -291,6 +291,7 @@ public class PageAdresses extends JFrame {
             }//fin boucle for
       //      tableauAdresse.clearSelection();
             displayRightTable();
+
             verifSiAjout = false;
         });
 
